0c17b671a2b6946f1459dd3d8dfe9a6dcf81f00b
Smily666 <edch66@gmail.com>
2022-09-24 16:07:07 -0400

    commit

---
+++ tut-2/updateRating.js
@@ -0,0 +1,24 @@
+const numStars = 15;
+
+function init(){
+    let stars = "Rating: ";
+    let buttons = "";
+    for (let i=1; i<=numStars; i++){
+        stars += `<span id="rating ${i}" style="color:blue">&#9733;</span>`;
+        buttons += `<button type="button" onclick="updateRating(${i})">Rate ${i}</button>`;
+    }
+    document.getElementById("stars").innerHTML = stars;
+    document.getElementById("buttons").innerHTML = buttons;
+}
+
+function updateRating(newRating){
+    for (let i=1; i<=numStars; i++){
+        let color;
+        if (i>newRating){
+            color = "lightgray";
+        }else{
+            color = "blue";
+        }
+        document.getElementById(`rating ${i}`).style.color = color;
+    }
+}
